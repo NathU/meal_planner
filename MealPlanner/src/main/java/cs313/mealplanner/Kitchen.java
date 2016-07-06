517860f7ea38b295c4fef08437df698d62f159b4
Nathan Ulmer <nathanulmer@gmail.com>
2016-07-06 13:02:52 -0600

    Servlet for page 5 done. Also, Kitchen.java is the new JDBC thing to use.

---
+++ MealPlanner/src/main/java/cs313/mealplanner/Kitchen.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs313.mealplanner;
+
+import java.util.*;
+import java.sql.*;
+
+/**
+ *
+ * @author nathanulmer
+ */
+public class Kitchen {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static private String DB_URL = "jdbc:mysql://localhost/kitchen";
+    static private String USER;
+    static private String PASS;
+    
+    
+    // Default constructor initializes basic parameters for THIS specific proj.
+    Kitchen () {
+        USER = "root";
+        PASS = "";
+        
+    }
+    
+    // This is really just for testing purposes on your own machine.
+    // Values for the working project (when we get it up on OpenShift)
+    //  should be hard-coded into the Default constructor above, OR
+    //  (more correctly) loaded in from a separate environment file.
+    Kitchen (String u, String p) {
+        USER = u;
+        PASS = p;
+        
+    }
+    /**
+     * getAccountInfo
+     * RETURNS a Map like this one:
+     * {
+     *  user_id = 3,
+     *  email = i8fudge@publicrestrooms.net,
+     *  password = asdf1234,
+     *  mealplan_id = 3,
+     *  user_info = {   name = Nate,
+     *                  dob = 10/20/1989, 
+     *                  gender = M, 
+     *                  height = 69, 
+     *                  weight = 150, 
+     *                  activity = ???,
+     *                  goal = -10 },
+     *  <something> = { <the meal plan Map...>}
+     * }
+     * 
+     */
+    // PS, don't forget your \" when building prepared statements.
+    public Map getAccountInfo (String email, String password) {
+        Map user_table_data = new HashMap();
+        Map meal_plan_table_data = new HashMap();
+        
+        user_table_data = retrieve("SELECT * FROM users WHERE users.email = \""+email+"\" AND users.password = \""+password+"\"");
+        user_table_data.put("user_id", user_table_data.get("id")); // "id" is going to be overwritten here in a sec...
+        
+        if (user_table_data.get("mealplan_id") != null) {
+            meal_plan_table_data = retrieve("SELECT * FROM meal_plans WHERE meal_plans.id = "+((String)user_table_data.get("mealplan_id"))+"");
+            user_table_data.putAll( meal_plan_table_data );
+            user_table_data.put("plan_id", meal_plan_table_data.get("id")); // "id" has been overwritten, so lets name it what it really is now...
+        }
+        
+        user_table_data.remove("id"); // and get rid of the redundant/ambiguously-named variable 'id'.
+
+        return user_table_data;
+    }
+    
+    /*
+    public Map getUserMealPlan (int userId) {
+        return retrieve("SELECT * FROM meal_plans WHERE meal_plans.id = (SELECT mealplan_id FROM users WHERE users.email = )");
+    }
+    */
+    
+    public Map getRecipe (int recipeId) {
+        return retrieve("SELECT * FROM recipes WHERE recipes.id = \""+recipeId+"\"");
+    }
+    
+    // TODO: finish this...
+    public int createNewMealPlan (int userId, Map plan) {
+        // First modify inserts plan into meal_plans table.
+        // Second modify updates users table with the newly inserted meal plan's id.
+        // Two rows should be affected after all this. If not, we're in trouble, so return 0.
+        int affected_rows = modify("") + modify("");
+        return affected_rows > 1 ? affected_rows : 0;
+    }
+    
+    public int insertNewUser (String name, String email, String password) {
+        Map my_name = new HashMap();
+        my_name.put("name", name);
+        return modify("INSERT INTO users (email, password, user_info) VALUES (\""+email+"\", \""+password+"\", \""+my_name+"\")");
+    }
+    
+    public int updateUserInfo (Map account_info) {
+        return modify("UPDATE users SET user_info = \""+((String)account_info.get("user_info"))+"\""+
+                " WHERE users.id = \""+((String)account_info.get("user_id"))+"\"");
+    }
+    
+    private int modify(String query) {
+        int affected_rows = 0;
+		
+        Connection conn = null;
+        Statement stmt = null;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            affected_rows = stmt.executeUpdate(query);
+        }
+        catch(SQLException se){ se.printStackTrace(); }
+        catch(Exception e){ e.printStackTrace(); }
+        
+        finally{
+            // clean up in case a throw occured.
+            try{
+             if(stmt!=null)
+                conn.close();
+            }catch(SQLException se){/*do nothin*/}
+            try{
+             if(conn!=null)
+                conn.close();
+            }catch(SQLException se){ se.printStackTrace(); }
+        }
+        
+        return affected_rows;
+    }
+    
+    
+    private Map retrieve (String query) {
+        Map query_results = new HashMap();
+        Connection conn = null;
+        Statement stmt = null;
+
+        try{
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    String columnValue = rs.getString(i);
+                    query_results.put(rsmd.getColumnName(i),columnValue);
+                }
+            }
+            rs.close();
+        }
+        catch(SQLException se){ se.printStackTrace(); }
+        catch(Exception e){ e.printStackTrace(); }
+        
+        finally{
+            // clean up in case a throw occured.
+            try{
+             if(stmt!=null)
+                conn.close();
+            }catch(SQLException se){/*do nothin*/}
+            try{
+             if(conn!=null)
+                conn.close();
+            }catch(SQLException se){ se.printStackTrace(); }
+        }
+        
+        return query_results;
+    }
+}
---
+++ MealPlanner/src/main/java/cs313/mealplanner/my_info.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs313.mealplanner;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nathanulmer
+ */
+@WebServlet(name = "my_info", urlPatterns = {"/my_info"})
+public class my_info extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet my_info</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3> This is just to test the servlet and make sure it works... </h3> <hr/>");
+            out.println("<p>All user profile info: <br/>" + request.getSession().getAttribute("profile_info") + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Kitchen kitchen = new Kitchen();
+        Map profile_info = new HashMap();
+        profile_info = kitchen.getAccountInfo(request.getParameter("username"), request.getParameter("password"));
+        request.getSession().setAttribute("profile_info", profile_info);       
+        
+        
+        //request.getRequestDispatcher("profile.jsp").forward(request, response);
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
